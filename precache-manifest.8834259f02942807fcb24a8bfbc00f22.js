9c9fd0ee031d7603ac657dbc9f155b32c939e8c8
minsungkang929 <minsung.kang929@gmail.com>
2019-08-21 09:32:58 -0400

    Updates

---
+++ precache-manifest.8834259f02942807fcb24a8bfbc00f22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f457da77656719d91669d68fe170dfaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723ebb6c20dfb3927a78",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "67400be2378e2237032a",
+    "url": "/static/js/2.fdbcfa46.chunk.js"
+  },
+  {
+    "revision": "723ebb6c20dfb3927a78",
+    "url": "/static/js/main.af326a27.chunk.js"
+  },
+  {
+    "revision": "da84832f4e7efa7dcebf",
+    "url": "/static/js/runtime~main.17ac85df.js"
+  }
+]);
